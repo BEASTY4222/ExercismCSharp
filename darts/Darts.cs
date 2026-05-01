8fedc6c05425c85fbded62d0e3c7798294868db3
Ivan Georgiev <ivangeorgiev181@gmail.com>
2026-05-01 23:14:38 +0300

    Darts DONE

--- darts/Darts.cs
+++ darts/Darts.cs
@@ -1,7 +1,14 @@
+using System;
 public static class Darts
 {
     public static int Score(double x, double y)
     {
-       throw new NotImplementedException("You need to implement this function");
+        int score = 0;
+
+        if(Math.Pow(x - 0,2) + Math.Pow(y - 0,2) <= 10 * 10)score = 1;
+        else if (Math.Pow(x - 0,2) + Math.Pow(y - 0,2) <= 5 * 5) score = 5;
+        else if(Math.Pow(x - 0,2) + Math.Pow(y - 0,2) <= 1) score = 10;
+
+       return score;
     }
 }
