90a3ca9ad280a453c94d27ff1d3cf602ef93fff2
Ivan Georgiev <ivangeorgiev181@gmail.com>
2026-05-03 21:34:26 +0300

    phone-number-analysis DONE

--- phone-number-analysis/PhoneNumberAnalysis.cs
+++ phone-number-analysis/PhoneNumberAnalysis.cs
@@ -2,11 +2,25 @@ public static class PhoneNumber
 {
     public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.Analyze() method");
+        (bool IsNewYork, bool IsFake, string LocalNumber) result;
+
+        string phoneNumberCopyIsNewYork = phoneNumber;
+        string phoneNumberCopyIsFake = phoneNumber;
+
+
+        if(phoneNumberCopyIsNewYork.Substring(0,3) == "212") result.IsNewYork = true;
+        else result.IsNewYork = false;
+
+        result.LocalNumber = phoneNumber.Substring(phoneNumber.Length - 4);
+
+        if (phoneNumberCopyIsFake.Substring(4, 3) == "555") result.IsFake = true;
+        else result.IsFake = false;
+
+        return result;
     }
 
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo)
     {
-        throw new NotImplementedException($"Please implement the (static) PhoneNumber.IsFake() method");
+        return phoneNumberInfo.IsFake;
     }
 }
