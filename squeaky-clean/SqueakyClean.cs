3d3d91645d6738b439fb025cb1bb89254b5b3851
Ivan Georgiev School <ivangeorgiev181@gmail.com>
2026-05-14 13:11:30 +0300

    squeaky-clean done

--- squeaky-clean/SqueakyClean.cs
+++ squeaky-clean/SqueakyClean.cs
@@ -1,18 +1,56 @@
-using System.Text.RegularExpressions;
+using System.Text;
 
 public static class Identifier
 {
     public static string Clean(string identifier)
     {
-        string result = identifier;
-
-        if (result == "") return "";
+        var result = new StringBuilder();
+        bool makeUpper = false;
         
-        if(result.contains(" "))  result = result.Replace(" ","_");
-        
-        result = Regex.Replace(result, @"\p{C}", "CTRL");
+        foreach (char c in identifier)
+        {
+            if (char.IsControl(c))
+            {
+                result.Append("CTRL");
+                continue;
+            }
+            if (c == ' ')
+            {
+                result.Append('_');
+                continue;
+            }
+            
+            // Skip kebab-case dash and capitalize next letter
+            if (c == '-')
+            {
+                makeUpper = true;
+                continue;
+            }
 
+            // Omit non-letters
+            if (!char.IsLetter(c))
+            {
+                continue;
+            }
 
+            // Omit Greek lowercase letters α to ω
+            if (c >= 'α' && c <= 'ω')
+            {
+                continue;
+            }
 
+            // Convert next character to uppercase after '-'
+            if (makeUpper)
+            {
+                result.Append(char.ToUpper(c));
+                makeUpper = false;
+            }
+            else
+            {
+                result.Append(c);
+            }
+        }
+
+        return result.ToString();
     }
 }
