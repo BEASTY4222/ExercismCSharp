3cb6851105685a2bc176180b95c0fc6d3449bcaf
Ivan Georgiev <ivangeorgiev181@gmail.com>
2026-05-17 17:06:40 +0300

    Binary-Search DONE

--- binary-search/BinarySearch.cs
+++ binary-search/BinarySearch.cs
@@ -2,6 +2,15 @@ public static class BinarySearch
 {
     public static int Find(int[] input, int value)
     {
-        throw new NotImplementedException("You need to implement this method.");
+        int left = 0, right = input.Length - 1, middle = 0;
+        while (left <= right)
+        {
+            middle = left + (right - left) / 2;
+            if(input[middle] < value) left = middle + 1;
+            else if(input[middle] > value) right = middle - 1;
+            else return middle;
+        }
+
+        return -1;
     }
 }
